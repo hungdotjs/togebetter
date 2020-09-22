b5d53c7ed5b275b9c6a537e91173874d06e130b5
Hung Tran <hungteddy98@gmail.com>
2020-09-22 14:51:54 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -13,7 +14,7 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/Login.vue'),
+    component: Login,
   },
   {
     path: '/signup',
